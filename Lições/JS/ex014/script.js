4712977a8f3c7be709ba7df745f35fa9b4f37b9a
LukasHMarques <dev.lucashmarques@outlook.com>
2022-02-02 11:11:05 -0300

    Ex014

--- Lições/JS/ex014/script.js
+++ Lições/JS/ex014/script.js
@@ -1,1 +1,21 @@
-alert("olá");
+function carregar() {
+    var msg = window.document.getElementById("msg");
+    var img = window.document.getElementById("imagem");
+    var data = new Date();
+    var hora = data.getHours();
+    msg.innerHTML = `Agora são ${hora} horas`
+
+    if (hora >= 0 && hora <= 12){
+        //Bom Dia!
+        img.src = "fotos/manhã.jpg";
+        document.body.style.background = "#e2cd9f";
+    } else if(hora < 18){
+        //Boa Tarde!
+        img.src = "fotos/tarde.jpg";
+        document.body.style.background = "green";
+    } else{
+        //Boa Noite!
+        img.src = "fotos/noite.jpg";
+        document.body.style.background = "black";
+    }
+}
